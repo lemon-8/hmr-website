261f02d94fde5d8fad8e9e59d83f2b7b5d5d6be7
Shashwat <shashwat4699@gmail.com>
2022-01-05 19:16:14 +0530

    connecting form and download link

--- components/HmrForm.js
+++ components/HmrForm.js
@@ -1,218 +1,180 @@
-import * as Yup from "yup";
-import { yupResolver } from "@hookform/resolvers/yup";
-import { useForm } from "react-hook-form";
-import "yup-phone";
+import * as Yup from 'yup';
+import { yupResolver } from '@hookform/resolvers/yup';
+import { useForm } from 'react-hook-form';
+import 'yup-phone';
 import {
-  FormControl,
-  FormLabel,
-  FormErrorMessage,
-  Textarea,
-} from "@chakra-ui/react";
-import { Input } from "@chakra-ui/react";
-import {
-  Box,
-  Heading,
-  HStack,
-  Text,
-  Stack,
-  VStack,
-  Flex,
-} from "@chakra-ui/layout";
-import { MdCall } from "react-icons/md";
-import { MdMail } from "react-icons/md";
-import { Image } from "@chakra-ui/react";
-import { Button, ButtonGroup } from "@chakra-ui/react";
-import { Link } from "@chakra-ui/react";
+	FormControl,
+	FormLabel,
+	FormErrorMessage,
+	Textarea,
+} from '@chakra-ui/react';
+import { Input } from '@chakra-ui/react';
+import { Box, Heading, HStack, Text, Stack, VStack } from '@chakra-ui/layout';
+import { MdCall, MdMail } from 'react-icons/md';
+import { Button, Image } from '@chakra-ui/react';
+import { Link } from '@chakra-ui/react';
 
-function HmrForm() {
-  return (
-    <Box
-      className="hmrForm"
-      padding={{ base: "5", xl: "9" }}
-      mx={{ base: "10", lg: "44", xl: "80" }}
-      mt={{ base: "5", lg: "20", xl: "32" }}
-      borderWidth="thin"
-      borderRadius={{ base: "2xl", xl: "2xl" }}
-    >
-      <Heading fontWeight="semibold" color="hmrblack.500">
-        Interested?
-      </Heading>
-      <Text fontWeight="500" color="hmrgray.500">
-        Fill the form below to know more or you can contact us on the mentioned
-        phone number
-      </Text>
-      <Stack direction={["column", "row"]}>
-        <Box w={{ base: "l", lg: "96", xl: "2xl" }}>
-          <FormControl id="name">
-            <FormLabel marginTop={{ base: "5", xl: "9" }}>Your Name</FormLabel>
-            <Input placeholder="Full Name" />
-            <FormLabel marginTop={{ base: "5", xl: "9" }}>Email ID</FormLabel>
-            <Input placeholder="Email ID" />
-            <FormLabel marginTop={{ base: "5", xl: "9" }}>
-              Phone Number
-            </FormLabel>
-            <Input placeholder="Phone Number" />
-            <FormLabel marginTop={{ base: "5", xl: "9" }}>
-              What can we help you with?
-            </FormLabel>
-            <Textarea placeholder="Description" />
-          </FormControl>
-        </Box>
-        <Box
-          // bg="red.100"
-          color="hmrblack.500"
-          paddingTop={{ base: "0", xl: "12" }}
-          paddingLeft={{ base: "0", lg: "10", xl: "10" }}
-          fontSize={{ base: "l", xl: "l" }}
-        >
-          <VStack alignItems="start" mt={{ base: "10", xl: "0" }}>
-            <HStack>
-              <MdCall color="#01CFE7" />
-              <Text>
-                <Link href="tel:91 98103-06346">+91 98103-06346</Link>
-              </Text>
-            </HStack>
-            <HStack marginTop={{ base: "0", xl: "7" }}>
-              <MdMail color="#01CFE7" />
-              <Text>
-                <Link href="mailto:hi@hmrhostels.com">hi@hmrhostels.com</Link>
-              </Text>
-            </HStack>
-          </VStack>
-          <Image
-            display={{ base: "none", xl: "initial" }}
-            src="thumbsup.png"
-            alt="thumbsup"
-          />
-        </Box>
-      </Stack>
-      <Button
-        colorScheme="hmrblack"
-        borderRadius="xl"
-        px="5"
-        fontSize={{ base: "l", md: "14", xl: "16" }}
-        marginTop={{ base: "5", xl: "9" }}
-        fontWeight="normal"
-        width={{ base: "100%", lg: "initial", xl: "initial" }}
-      >
-        Submit
-      </Button>
-    </Box>
-  );
-}
+export default function HmrForm() {
+	const schema = Yup.object().shape({
+		name: Yup.string()
+			.matches(/^[A-Za-z ]*$/, 'Please enter valid name')
+			.min(6, 'Please enter your full name')
+			.required('Name is required'),
+		email: Yup.string().email('Invalid email'),
+		phone: Yup.string()
+			.required('You cannot procceed without a valid phone number')
+			.phone('IN', false, 'Please enter a valid phone number'),
+		description: Yup.string(),
+	});
 
-export default function ContactForm() {
-  const schema = Yup.object().shape({
-    name: Yup.string()
-      .matches(/^[A-Za-z ]*$/, "Please enter valid name")
-      .min(6, "Please enter your full name")
-      .required("Name is required"),
-    email: Yup.string().email("Invalid email").required("Email is required"),
-    phone: Yup.string()
-      .required("You cannot procceed without a valid phone number")
-      .phone("IN", false, "Please enter a valid phone number"),
-    description: Yup.string(),
-  });
+	const {
+		register,
+		handleSubmit,
+		formState: { errors, isSubmitting },
+	} = useForm({
+		resolver: yupResolver(schema),
+		mode: 'onBlur',
+	});
 
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm({
-    resolver: yupResolver(schema),
-  });
+	const onSubmit = (data) => {
+		console.log(data, 'data');
+		const dictMapping = {
+			name: 'entry.1965918628',
+			email: 'entry.1322283561',
+			phone: 'entry.1002340236',
+			description: 'entry.509553721',
+		};
 
-  const onSubmit = (data) => {
-    console.log(data, "data");
-  };
-  console.log(errors, "errors");
-  return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <Box
-        className="hmrForm"
-        padding={{ base: "5", xl: "9" }}
-        mx={{ base: "10", lg: "44", xl: "80" }}
-        mt={{ base: "5", lg: "20", xl: "32" }}
-        borderWidth="thin"
-        borderRadius={{ base: "2xl", xl: "2xl" }}
-      >
-        <Heading fontWeight="semibold" color="hmrblack.500">
-          Interested?
-        </Heading>
-        <Text fontWeight="500" color="hmrgray.500">
-          Fill the form below to know more or you can contact us on the
-          mentioned phone number
-        </Text>
-        <Stack direction={["column", "row"]}>
-          <Box w={{ base: "l", lg: "96", xl: "2xl" }}>
-            <FormControl isInvalid={errors.name}>
-              <FormLabel marginTop={{ base: "5", xl: "9" }}>
-                Your Name
-              </FormLabel>
-              <Input placeholder="Full Name" {...register("name")} />
-              <FormErrorMessage>
-                {errors.name && errors.name.message}
-              </FormErrorMessage>
-            </FormControl>
-            <FormControl>
-              <FormLabel marginTop={{ base: "5", xl: "9" }}>Email ID</FormLabel>
-              <Input placeholder="Email ID" {...register("email")} />
-            </FormControl>
-            <FormControl>
-              <FormLabel marginTop={{ base: "5", xl: "9" }}>
-                Phone Number
-              </FormLabel>
-              <Input placeholder="Phone Number" {...register("phone")} />
-            </FormControl>
-            <FormControl>
-              <FormLabel marginTop={{ base: "5", xl: "9" }}>
-                What can we help you with?
-              </FormLabel>
-              <Textarea
-                placeholder="Description"
-                {...register("description")}
-              />
-            </FormControl>
-          </Box>
-          <Box
-            color="hmrblack.500"
-            paddingTop={{ base: "0", xl: "12" }}
-            paddingLeft={{ base: "0", lg: "10", xl: "10" }}
-            fontSize={{ base: "l", xl: "l" }}
-          >
-            <VStack alignItems="start" mt={{ base: "10", xl: "0" }}>
-              <HStack>
-                <MdCall color="#01CFE7" />
-                <Text>
-                  <Link href="tel:91 98103-06346">+91 98103-06346</Link>
-                </Text>
-              </HStack>
-              <HStack marginTop={{ base: "0", xl: "7" }}>
-                <MdMail color="#01CFE7" />
-                <Text>
-                  <Link href="mailto:hi@hmrhostels.com">hi@hmrhostels.com</Link>
-                </Text>
-              </HStack>
-            </VStack>
-            <Image
-              display={{ base: "none", xl: "initial" }}
-              src="thumbsup.png"
-              alt="thumbsup"
-            />
-          </Box>
-        </Stack>
-        <Button
-          colorScheme="hmrblack"
-          borderRadius="xl"
-          px="5"
-          fontSize={{ base: "l", md: "14", xl: "16" }}
-          marginTop={{ base: "5", xl: "9" }}
-          fontWeight="normal"
-          width={{ base: "100%", lg: "initial", xl: "initial" }}
-        >
-          Submit
-        </Button>
-      </Box>
-    </form>
-  );
+		let baseUrl =
+			'https://docs.google.com/forms/d/e/1FAIpQLSeJHzc7FMxGuJjQzVFW6hGd8MyUpjGbIDNacuqZgvFDnQlmEw/formResponse?usp=pp_url&submit=Submit';
+
+		// Iterate over dictMapping and check if key exists in values
+		Object.keys(dictMapping).forEach((key) => {
+			baseUrl += '&' + dictMapping[key] + '=';
+			if (data[key]) {
+				// If key exists, add to the form values
+				baseUrl += data[key].replace('+', '%2B').replace(' ', '+');
+			}
+		});
+
+		console.log(baseUrl);
+		fetch(baseUrl)
+			.then((response) => console.log(response))
+			.catch((e) => console.log('Form Error', e));
+	};
+
+	return (
+		<form onSubmit={handleSubmit(onSubmit)}>
+			<Box
+				className="hmrForm"
+				padding={{ base: '5', xl: '9' }}
+				mx={{ base: '10', lg: '44', xl: '80' }}
+				mt={{ base: '5', lg: '20', xl: '32' }}
+				borderWidth="thin"
+				borderRadius={{ base: '2xl', xl: '2xl' }}
+			>
+				<Heading fontWeight="semibold" color="hmrblack.500">
+					Interested?
+				</Heading>
+				<Text fontWeight="500" color="hmrgray.500">
+					Fill the form below to know more or you can contact us on
+					the mentioned phone number
+				</Text>
+				<Stack direction={['column', 'row']}>
+					<Box w={{ base: 'l', lg: '96', xl: '2xl' }}>
+						<FormControl isInvalid={errors.name} isRequired>
+							<FormLabel marginTop={{ base: '5', xl: '9' }}>
+								Your Name
+							</FormLabel>
+							<Input
+								placeholder="Full Name"
+								{...register('name')}
+							/>
+							<FormErrorMessage>
+								{errors.name && errors.name.message}
+							</FormErrorMessage>
+						</FormControl>
+						<FormControl isInvalid={errors.email}>
+							<FormLabel marginTop={{ base: '5', xl: '9' }}>
+								Email ID
+							</FormLabel>
+							<Input
+								placeholder="Email ID"
+								type="email"
+								{...register('email')}
+							/>
+							<FormErrorMessage>
+								{errors.email && errors.email.message}
+							</FormErrorMessage>
+						</FormControl>
+						<FormControl isInvalid={errors.phone} isRequired>
+							<FormLabel marginTop={{ base: '5', xl: '9' }}>
+								Phone Number
+							</FormLabel>
+							<Input
+								placeholder="Phone Number"
+								type="tel"
+								{...register('phone')}
+							/>
+							<FormErrorMessage>
+								{errors.phone && errors.phone.message}
+							</FormErrorMessage>
+						</FormControl>
+						<FormControl>
+							<FormLabel marginTop={{ base: '5', xl: '9' }}>
+								What can we help you with?
+							</FormLabel>
+							<Textarea
+								placeholder="Description"
+								{...register('description')}
+							/>
+						</FormControl>
+					</Box>
+					<Box
+						color="hmrblack.500"
+						paddingTop={{ base: '0', xl: '12' }}
+						paddingLeft={{ base: '0', lg: '10', xl: '10' }}
+						fontSize={{ base: 'l', xl: 'l' }}
+					>
+						<VStack alignItems="start" mt={{ base: '10', xl: '0' }}>
+							<HStack>
+								<MdCall color="#01CFE7" />
+								<Text>
+									<Link href="tel:91 98103-06346">
+										+91 98103-06346
+									</Link>
+								</Text>
+							</HStack>
+							<HStack marginTop={{ base: '0', xl: '7' }}>
+								<MdMail color="#01CFE7" />
+								<Text>
+									<Link href="mailto:hi@hmrhostels.com">
+										hi@hmrhostels.com
+									</Link>
+								</Text>
+							</HStack>
+						</VStack>
+						<Image
+							display={{ base: 'none', xl: 'initial' }}
+							src="thumbsup.png"
+							alt="thumbsup"
+						/>
+					</Box>
+				</Stack>
+				<Button
+					colorScheme="hmrblack"
+					borderRadius="xl"
+					px="5"
+					fontSize={{ base: 'l', md: '14', xl: '16' }}
+					marginTop={{ base: '5', xl: '9' }}
+					fontWeight="normal"
+					width={{ base: '100%', lg: 'initial', xl: 'initial' }}
+					type="submit"
+					isLoading={isSubmitting}
+				>
+					Submit
+				</Button>
+			</Box>
+		</form>
+	);
 }
--- components/Navbar.js
+++ components/Navbar.js
@@ -1,118 +1,129 @@
-import { HStack } from "@chakra-ui/layout";
+import { HStack } from '@chakra-ui/layout';
 import {
-  Menu,
-  MenuButton,
-  MenuList,
-  MenuItem,
-  MenuDivider,
-} from "@chakra-ui/react";
-import { IconButton } from "@chakra-ui/react";
-import { HiMenuAlt4 } from "react-icons/hi";
-import { Image } from "@chakra-ui/react";
-import { MdFastfood, MdPhoto } from "react-icons/md";
-import { scroller } from "react-scroll";
-import { MdFileDownload } from "react-icons/md";
-import { MdPhone } from "react-icons/md";
-import { FaConciergeBell } from "react-icons/fa";
-import { BsPhoneFill } from "react-icons/bs";
+	Menu,
+	MenuButton,
+	MenuList,
+	MenuItem,
+	MenuDivider,
+} from '@chakra-ui/react';
+import { IconButton } from '@chakra-ui/react';
+import { HiMenuAlt4 } from 'react-icons/hi';
+import { Image } from '@chakra-ui/react';
+import { MdFastfood, MdPhoto } from 'react-icons/md';
+import { scroller } from 'react-scroll';
+import { MdFileDownload } from 'react-icons/md';
+import { MdPhone } from 'react-icons/md';
+import { FaConciergeBell } from 'react-icons/fa';
+import { BsPhoneFill } from 'react-icons/bs';
+import { useRouter } from 'next/router';
 
 export default function NavBar() {
-  return (
-    <HStack
-      zIndex="1"
-      bg={{ base: "white", md: "transparent", lg: "transparent" }}
-      pt={{ base: "6", md: "2", lg: "12" }}
-      px={{ base: "6", md: "2", lg: "28" }}
-      pb={{ base: "6", md: "2", lg: "12" }}
-      justifyContent="space-between"
-      width="100%"
-      minHeight="16"
-      position="fixed"
-    >
-      <Image
-        cursor="pointer"
-        onClick={hero_section}
-        width={{ base: "8", md: "8", lg: "12" }}
-        src="hmrlogox100.png"
-        alt="hmr logo"
-      ></Image>
+	const router = useRouter();
 
-      <Menu>
-        <MenuButton
-          as={IconButton}
-          variant="solid"
-          aria-label="Options"
-          icon={<HiMenuAlt4 />}
-        />
-        <MenuList>
-          <MenuItem icon={<MdFastfood />} onClick={games_section}>
-            Facilities
-          </MenuItem>
-          <MenuItem icon={<MdPhoto />} onClick={slideshow_photos}>
-            Photos
-          </MenuItem>
-          <MenuItem icon={<FaConciergeBell />} onClick={scrollToSection}>
-            Ammenities
-          </MenuItem>
+	return (
+		<HStack
+			zIndex="1"
+			bg={{ base: 'white', md: 'transparent', lg: 'transparent' }}
+			pt={{ base: '6', md: '2', lg: '12' }}
+			px={{ base: '6', md: '2', lg: '28' }}
+			pb={{ base: '6', md: '2', lg: '12' }}
+			justifyContent="space-between"
+			width="100%"
+			minHeight="16"
+			position="fixed"
+		>
+			<Image
+				cursor="pointer"
+				onClick={hero_section}
+				width={{ base: '8', md: '8', lg: '12' }}
+				src="hmrlogox100.png"
+				alt="hmr logo"
+			></Image>
 
-          <MenuItem icon={<BsPhoneFill />} onClick={ourApp}>
-            Our App
-          </MenuItem>
-          <MenuDivider />
-          <MenuItem icon={<MdFileDownload />}>Download App</MenuItem>
-          <MenuItem icon={<MdPhone />} onClick={hmrForm}>
-            Contact Us
-          </MenuItem>
-        </MenuList>
-      </Menu>
-    </HStack>
-  );
+			<Menu>
+				<MenuButton
+					as={IconButton}
+					variant="solid"
+					aria-label="Options"
+					icon={<HiMenuAlt4 />}
+				/>
+				<MenuList>
+					<MenuItem icon={<MdFastfood />} onClick={games_section}>
+						Facilities
+					</MenuItem>
+					<MenuItem icon={<MdPhoto />} onClick={slideshow_photos}>
+						Photos
+					</MenuItem>
+					<MenuItem
+						icon={<FaConciergeBell />}
+						onClick={scrollToSection}
+					>
+						Ammenities
+					</MenuItem>
+
+					<MenuItem icon={<BsPhoneFill />} onClick={ourApp}>
+						Our App
+					</MenuItem>
+					<MenuDivider />
+					<MenuItem
+						icon={<MdFileDownload />}
+						onClick={() => router.push('/download')}
+					>
+						Download App
+					</MenuItem>
+					<MenuItem icon={<MdPhone />} onClick={hmrForm}>
+						Contact Us
+					</MenuItem>
+				</MenuList>
+			</Menu>
+		</HStack>
+	);
 }
 
 const scrollToSection = () => {
-  scroller.scrollTo("Ammenities_section", {
-    duration: 800,
-    delay: 0,
-    smooth: "easeInOutQuart",
-  });
+	scroller.scrollTo('Ammenities_section', {
+		duration: 800,
+		delay: 0,
+		smooth: 'easeInOutQuart',
+	});
 };
 
 const ourApp = () => {
-  scroller.scrollTo("ourApp", {
-    duration: 800,
-    delay: 0,
-    smooth: "easeInOutQuart",
-  });
+	scroller.scrollTo('ourApp', {
+		duration: 800,
+		delay: 0,
+		smooth: 'easeInOutQuart',
+	});
 };
 
 const hmrForm = () => {
-  scroller.scrollTo("hmrForm", {
-    duration: 800,
-    delay: 0,
-    smooth: "easeInOutQuart",
-  });
+	scroller.scrollTo('hmrForm', {
+		duration: 800,
+		delay: 0,
+		smooth: 'easeInOutQuart',
+	});
 };
 
 const slideshow_photos = () => {
-  scroller.scrollTo("slideshow_photos", {
-    duration: 800,
-    delay: 0,
-    smooth: "easeInOutQuart",
-  });
+	scroller.scrollTo('slideshow_photos', {
+		duration: 800,
+		delay: 0,
+		smooth: 'easeInOutQuart',
+	});
 };
 
 const games_section = () => {
-  scroller.scrollTo("games_section", {
-    duration: 800,
-    delay: 0,
-    smooth: "easeInOutQuart",
-  });
+	scroller.scrollTo('games_section', {
+		duration: 800,
+		delay: 0,
+		smooth: 'easeInOutQuart',
+	});
 };
 
 const hero_section = () => {
-  scroller.scrollTo("hero_section", {
-    duration: 800,
-    delay: 0,
-    smooth: "easeInOutQuart",
-  });
+	scroller.scrollTo('hero_section', {
+		duration: 800,
+		delay: 0,
+		smooth: 'easeInOutQuart',
+	});
 };
