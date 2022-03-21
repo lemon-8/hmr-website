041d38d6f10f3d572a055f2166e6d30b321e97e4
Abhimanyu Jha <abhimanyu.in@gmail.com>
2022-03-21 13:25:58 +0530

    privacy and terms

--- next.config.js
+++ next.config.js
@@ -1,3 +1,13 @@
 module.exports = {
-  reactStrictMode: true,
-}
+	reactStrictMode: true,
+	async redirects() {
+		return [
+			{
+				source: "/pricing",
+				destination:
+					"https://docs.google.com/spreadsheets/d/1RTB2UYEo-d7VcEVZbWHLyQumtwxaoGowjkLVtHu7_cc/edit#gid=0",
+				permanent: true,
+			},
+		];
+	},
+};
