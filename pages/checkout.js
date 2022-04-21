5444c0f6c9bb637d2bcd208fe3ab902f2f7386dc
Shashwat <shashwat4699@gmail.com>
2022-04-21 17:34:06 +0530

    fix(checkout): updating checkout redirect button

--- pages/checkout.js
+++ pages/checkout.js
@@ -1,10 +1,11 @@
-import React, { useEffect } from "react";
-import { useRouter } from "next/router";
-import { Button, Center, Heading, VStack } from "@chakra-ui/react";
+import React, { useEffect, useState, useRef } from 'react';
+import { useRouter } from 'next/router';
+import { Button, Center, Heading, VStack } from '@chakra-ui/react';
+import Link from 'next/link';
 
 function loadScript(src) {
 	return new Promise((resolve) => {
-		const script = document.createElement("script");
+		const script = document.createElement('script');
 		script.src = src;
 		script.onload = () => {
 			resolve(true);
@@ -19,12 +20,15 @@ function loadScript(src) {
 export default function Checkout() {
 	const route = useRouter();
 	const { order_id, redirect_url } = route.query;
+	const [redirectURL, setRedicrectURL] = useState(null);
+	const buttonRef = useRef(null);
+
 	async function displayRazorpay(order_id, redirect_url) {
 		const res = await loadScript(
-			"https://checkout.razorpay.com/v1/checkout.js"
+			'https://checkout.razorpay.com/v1/checkout.js'
 		);
 		if (!res) {
-			alert("Razorpay SDK failed to load. Are you online?");
+			alert('Razorpay SDK failed to load. Are you online?');
 			return;
 		}
 
@@ -32,19 +36,14 @@ export default function Checkout() {
 			key: process.env.NEXT_PUBLIC_RAZORPAY_KEY,
 			order_id: order_id,
 			theme: {
-				color: "#242535",
+				color: '#242535',
 			},
-			name: "HMR Hostels",
-			send_sms_hash: "true",
+			name: 'HMR Hostels',
+			send_sms_hash: 'true',
 			handler: function (response) {
 				const hmr = `${redirect_url}/?orderId=${response.razorpay_order_id}&paymentId=${response.razorpay_payment_id}&signature=${response.razorpay_signature}/`;
-				let a = document.createElement("a");
-				a.target = "_blank";
-				a.href = hmr;
-				a.innerText = "HMR";
-				a.id = "hmr";
-				let container = document.getElementsByClassName("checkout")[0];
-				container.appendChild(a);
+				setRedicrectURL(hmr);
+				buttonRef.current.click();
 			},
 		};
 		const paymentObject = new window.Razorpay(options);
@@ -59,7 +58,11 @@ export default function Checkout() {
 		<Center className="checkout" h="100vh">
 			<VStack spacing="12">
 				<Heading>HMR Hostels</Heading>
-				<Button>Go Back to App</Button>
+				<Link href={redirectURL ?? ''} passHref>
+					<Button disabled={!redirectURL} ref={buttonRef}>
+						Go Back to App
+					</Button>
+				</Link>
 			</VStack>
 		</Center>
 	);
